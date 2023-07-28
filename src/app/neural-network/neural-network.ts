48ad5024e96c67fe40dd97ce5ddece108108346e
Alexander <DrA1ex@users.noreply.github.com>
2023-07-28 19:29:09 +0500

    Export matrix && iter

--- src/app/neural-network/neural-network.ts
+++ src/app/neural-network/neural-network.ts
@@ -4,6 +4,8 @@ import {Activations} from "./engine/activations";
 import {Models} from "./engine/models";
 import {Initializers} from "./engine/initializers";
 import * as Utils from "./utils"
+import * as Matrix from "./engine/matrix"
+import * as Iter from "./engine/iter"
 
 export default {Activations, Initializers, Optimizers, Layers, Models, Utils};
-export {Activations, Initializers, Optimizers, Layers, Models, Utils};
+export {Activations, Initializers, Optimizers, Layers, Models, Utils, Matrix, Iter};
