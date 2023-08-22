bb531a7d09d6172c99e96a15be21d07eec11488e
Alexander <DrA1ex@users.noreply.github.com>
2023-08-22 20:02:40 +0500

    Add cartoon animation example

---
+++ examples/src/cartoon_animation_example.js
@@ -0,0 +1,48 @@
+import fs from "fs";
+import {ModelSerialization, GanSerialization, Matrix,} from "mind-net.js";
+import * as ImageUtils from "./utils/image.js";
+import * as ModelUtils from "./utils/model.js";
+
+
+const name = "2023-08-22T12:45:42.929Z_40";
+const path = "./_out/_models";
+
+const vaeDump = fs.readFileSync(`${path}/vae_${name}.json`);
+const upscalerDump = fs.readFileSync(`${path}/upscaler_${name}.json`);
+const ganDump = fs.readFileSync(`${path}/gan_${name}.json`);
+
+const vae = ModelSerialization.load(JSON.parse(vaeDump.toString()));
+const upscaler = ModelSerialization.load(JSON.parse(upscalerDump.toString()));
+const gan = GanSerialization.load(JSON.parse(ganDump.toString()));
+
+const count = 20;
+const framesPerSample = 4;
+
+const inSize = gan.generator.layers[0].size;
+const outSize = Math.sqrt(upscaler.layers[upscaler.layers.length - 1].size);
+
+const beginning = Matrix.random_normal_1d(inSize, -1, 1);
+let start = beginning;
+for (let k = 0; k <= count; k++) {
+    let next;
+    if (k === 10) {
+        next = beginning;
+    } else {
+        next = Matrix.random_normal_1d(inSize, -1, 1);
+    }
+
+    for (let i = 0; i <= framesPerSample; i++) {
+        const data = start.map((v, k) => v + (next[k] - v) * i / framesPerSample);
+
+        await ImageUtils.saveImageGrid(() => {
+                const gen = gan.generator.compute(data);
+                const filtered = ModelUtils.processMultiChannelData(vae, gen, 3, gen);
+                return ModelUtils.processMultiChannelData(upscaler, filtered, 3);
+            },
+            `./out/animation/animation_${(k * framesPerSample + i).toString().padStart(6, "0")}.png`,
+            outSize, 1, 3, 0, 1
+        );
+    }
+
+    start = next;
+}
